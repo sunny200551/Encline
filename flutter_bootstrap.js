caf314f3d23284f28a4914e2f28494c78fd7fb0b
sunny200551 <sunny.pasumarthi9640@gmail.com>
2026-06-04 06:40:51 +0000

    Deploying to gh-pages from @ sunny200551/Encline@da2eb09800c051d8248232bfe7d3c8ccde4ce6fd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3359027246" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "949925705" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
